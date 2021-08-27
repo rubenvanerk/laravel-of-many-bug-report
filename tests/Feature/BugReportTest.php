fcb8c8b44fa254e5cc1b3ae7487252206d091663
Ruben van Erk <ruben@rubenvanerk.nl>
2021-08-27 15:20:23 +0200

    Add test

--- tests/Feature/BugReportTest.php
+++ tests/Feature/BugReportTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Models\Brand;
+use App\Models\Category;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -18,6 +19,19 @@ class BugReportTest extends TestCase
     public function test_bug_report()
     {
         $this->seed();
+
+        $cheapestProduct = Brand::query()->first()->cheapestProduct;
+
+        $this->assertTrue($cheapestProduct->category->published);
+    }
+
+    public function test_bug_report_inverted()
+    {
+        $this->seed();
+
+        foreach (Category::all() as $category) {
+            $category->update(['published' => !$category->published]);
+        }
 
         $cheapestProduct = Brand::query()->first()->cheapestProduct;
 
